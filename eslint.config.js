846ae2ffe82b505763466b496fe7e587181a265c
codesbyree <bahreesaepul1@gmail.com>
2026-02-04 10:26:57 +0700

    upated eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,18 @@
-// https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 
 module.exports = defineConfig([
-  expoConfig,
+  ...expoConfig, // Spread the array if expoConfig returns one
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {
+      // Disable rules that don't play nice with TS types
+      'no-undef': 'off',
+      'no-unused-vars': 'off',
+      // Enable the TypeScript-aware version of unused vars
+      '@typescript-eslint/no-unused-vars': ['error'],
+    },
+  },
   {
     ignores: ['dist/*'],
   },
